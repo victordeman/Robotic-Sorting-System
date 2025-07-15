548bc3cc49b8b6240e70bec6057ad23200995a9c
Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2025-07-15 14:14:32 +0200

    Update sorting_log.sql

--- src/database/sorting_log.sql
+++ src/database/sorting_log.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS SortingLog (
     object_type TEXT,
     status TEXT
 );CREATE TABLE SortingLog (id INTEGER PRIMARY KEY AUTOINCREMENT, object_type TEXT, timestamp DATETIME);
+
+
+
+#---cd ~/Downloads/Robotic-Sorting-System-main sqlite3 src/database/sorting_log.db < src/database/sorting_log.sql
+#---ls -l src/database/sorting_log.db
