dd81c36a3b0380a0cb1bd0b26465247303e67090
Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2025-07-15 12:42:57 +0200

    Update robot_config.py

--- robodk/robot_config.py
+++ robodk/robot_config.py
@@ -1,1 +1,4 @@
-# Python API for RoboDK control\nprint('RoboDK configuration placeholder')
+from robodk import robolink
+RDK = robolink.Robolink()
+robot = RDK.Item('Kuka', robolink.ITEM_TYPE_ROBOT)
+robot.MoveJ([0, 0, 0, 0, 0, 0])  # Example movement
